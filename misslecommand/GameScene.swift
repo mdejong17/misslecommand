f8f9584db2ca53b3b03b6999d8135aabc19250d1
md17 <keeleydejong17@gmail.com>
2023-01-05 13:14:57 -0500

    Added everything

--- misslecommand/GameScene.swift
+++ misslecommand/GameScene.swift
@@ -9,80 +9,71 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var missile = SKSpriteNode()
+    var num = 0.0
+    var touchedBefore = false
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        //add border around the scene
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody = border
+//        let yellowBox = CGRect(x: 0, y: 0, width: 40, height: 40)
+//        let touchPoint = touch.location(in: bubbleNode)
+//        if yellowBox.contains(touch) {
+//           print("Yellow box is touched!")
+//        }
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        armada()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        
+        let location = touches.first!.location(in: self)
+        
+//        if !touchedBefore {
+//            touchedBefore = true
+            let missile = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 100, height: 100))
+            addChild(missile)
+        missile.move(location)
+
+//        }
+//        else {
+//                // do your touch handling for other times
+//            }
+//
+    }
+    func createMissile()
+    {
+        let missile2 = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 100, height: 100))
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    
+    func armada()
+    {
+        let wait = SKAction.wait(forDuration: 0.5)
+        let sequence = SKAction.sequence([wait, SKAction.run(createShip)])
+        run(SKAction.repeatForever(sequence))
+    }
+     
+    func createShip()
+    {
+        let randx = Double.random(in: 0...1)
+        let possibledx = [75, 100, -75, -100]
+        let randdx =  possibledx.randomElement()!
+        
+        let ship = SKSpriteNode(color: UIColor.white, size: CGSize(width: 100, height: 100))
+        ship.position = CGPoint(x: frame.width * randx, y: frame.height - 100)
+        addChild(ship)
+        ship.physicsBody = SKPhysicsBody(rectangleOf: ship.frame.size)
+        ship.physicsBody?.applyImpulse(CGVector(dx: randdx, dy: 300))
+        
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
+
---
+++ test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  misslecommand
+//
+//  Created by  on 1/3/23.
+//
+
+import Foundation
